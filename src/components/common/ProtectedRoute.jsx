8eb2880aa904dc7dc1be9c6743aa257eff177e67
quietjun802 <whdydwns802@gmail.com>
2025-12-04 17:12:20 +0900

    Team main (#15)
    * 메인 홈페이지 구조 조금 만졌어요
    
    * frontend almost end
    
    * frontend user 진짜 끝
    
    * 국장님 한번 해볼게
    
    * 1
    
    * forntend 이미지 바꿈, 로그인 회원가입 페이지 색 변경
    
    * 로그인 페이지 비즈니스 회원가입 추가
    
    * 로그인 페이지 비즈니스 회원가입 추가
    
    * 1
    
    * 1
    
    * 현석아 도커 해봐
    
    * 도커 연결 해봅시다 국장님
    
    * temp: working changes
    
    * first 가져옴
    
    * 1
    
    * main으로 넘어가서 작업합니다
    
    * 마이페이지, 호텔검색 뼈대 완성
    
    * 호텔 리스트와 디자인 끝나고 호텔디테일(예약) 디자인 초반단계
    
    * Remove node_modules from tracking
    
    * hotel-page 수정 및 SCSS 충돌 해결
    
    * 이번에 안되면 개발자 안합니다

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,26 +1,14 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-<<<<<<< HEAD
-import { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
-
-  // 로그인 안 됨 → /login 으로 이동
-  if (!user) {
-=======
 import { AuthContext } from "../../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthed } = useContext(AuthContext);
 
   if (!isAuthed) {
->>>>>>> main
     return <Navigate to="/login" replace />;
   }
 
-  // 로그인 됨 → children 보여줌
   return children;
 };
 
